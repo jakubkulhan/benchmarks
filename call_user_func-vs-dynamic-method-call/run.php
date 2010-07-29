b1a94cf25721eb3ff43f132ee348f14ed3429903
Jakub Kulhan <jakub.kulhan@gmail.com>
2010-07-29 13:27:46 +0200

    Typo

--- call_user_func-vs-dynamic-method-call/run.php
+++ call_user_func-vs-dynamic-method-call/run.php
@@ -6,7 +6,7 @@ header('Content-Type: text/plain; charset=UTF-8');
 Benchmark: call_user_func(array($object, $method)) vs. $object->$method()
 -------------------------------------------------------------------------
 <?php
-echo "call_user_func:  ";
+echo "call_user_func:      ";
 require_once dirname(__FILE__) . '/call_user_func.phps';
 
 echo "dynamic method call: ";
